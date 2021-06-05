a3b447585b931a963a87f2c9ec30743788a926d1
Vuong Bui <btv11b1@gmail.com>
2021-06-05 17:05:17 +0700

    main

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
     Event_Click_Nav_Icon();
     Event_Click_Out_Main();
-    //Event_Click_Link_Redirect();
+    Event_Click_Link_Redirect();
     //Event_Click_Link_Redirect_First_Link();
 })
 
 
 ////#region // Event Click Show Hide Menu
-function Event_Click_Nav_Icon(){
+function Event_Click_Nav_Icon() {
     let navicon = $("#header_navicon");
-    navicon.on("click",function(){
+    navicon.on("click", function () {
         $("#menu_list").toggleClass("wrap");
         $("#menu,#main").toggleClass("wrap")
     })
@@ -18,13 +18,13 @@ function Event_Click_Nav_Icon(){
 
 
 ////#region // Event Click Out 
-function Event_Click_Out_Main(){
-    $(document).on("mousedown",function(e){
-        if($(window).width() < 800){
+function Event_Click_Out_Main() {
+    $(document).on("mousedown", function (e) {
+        if ($(window).width() < 800) {
             let menu = $("#menu")
             let navicon = $("#header_navicon");
-            if ( (!menu.is(e.target) && menu.has(e.target).length === 0 )
-            && (!navicon.is(e.target) && navicon.has(e.target).length === 0) ){
+            if ((!menu.is(e.target) && menu.has(e.target).length === 0)
+                && (!navicon.is(e.target) && navicon.has(e.target).length === 0)) {
                 $("#menu,#main").removeClass("wrap");
             }
         }
@@ -33,14 +33,20 @@ function Event_Click_Out_Main(){
 ////#endregion
 
 ////#region // Event Click Out 
-function Event_Click_Link_Redirect(){
-    $("#menu_list").on("click", '.menu_item', function(e){
-         let link = $(this).attr("data-href");
-         if(link != "")
-            $("#main_section").load(link);
+function Event_Click_Link_Redirect() {
+    $("#menu_list").on("click", '.menu_item', function (e) {
+        let link = $(this).attr("data-href");
+        if (link != "") {
+            $("#box-loading").show();
+            setTimeout(function () {
+                $("#main_section").load(link, function () {
+                    $("#box-loading").hide();
+                });
+            }, 2000)
+        }
     });
 }
-function Event_Click_Link_Redirect_First_Link(){
+function Event_Click_Link_Redirect_First_Link() {
     $("#menu_list .menu_item:nth-child(2)").click();
 }
 ////#endregion
